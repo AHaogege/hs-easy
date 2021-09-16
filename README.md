90af86768f2e16ed38c6777da578a6f97b508bee
AHaogege <1119515353@qq.com>
2021-09-16 21:16:59 +0800

    提交代码

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # hs-easy
+ *整理前端开发过程中用到的一些常用方法，持续更新
+ *深拷贝（deepCopy)
+ *本地存储（getStorage，setStorage，removeStorage）
+# 使用方式
+ *npm i hs-easy
+
+ *举例
+
+import hsEasy from 'hs-easy'
+
+hsEasy.common.deepCopy({data})
+ 
--- src/common.js
+++ src/common.js
@@ -1,7 +1,22 @@
 // 前端常用深拷贝
 const deepCopy = (object)=>{
     return JSON.parse(JSON.stringify(object))
+},
+// localstorage 存取
+const getStorage = (name)=>{
+    return JSON.parse(window.localStorage.getItem(name))||''
+},
+// 设置
+const setStorage = (name,data)=>{
+    window.localStorage.setItem(name,JSON.stringify(data))
+},
+// 清除指定
+const removeStorage =(name)=>{
+    window.localStorage.removeItem(name)
 }
 module.exports = {
-    deepCopy
+    deepCopy,
+    getStorage,
+    setStorage,
+    removeStorage
 }
