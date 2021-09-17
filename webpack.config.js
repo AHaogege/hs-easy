9e089441954ac7aabde0c287dd7a31eb8b9c47e5
AHaogege <1119515353@qq.com>
2021-09-17 21:32:17 +0800

    提交修改

--- webpack.config.js
+++ webpack.config.js
@@ -9,20 +9,20 @@ module.exports={
     output:{
         filename:'[name].js',    //输入的文件名是什么，生成的文件名也是什么
         publicPath:'/dist/',
-        // library:'hsEasy',
-        // libraryTarget:'umd',
-        // umdNamedDefine:false,
+        library:'hsEasy',
+        libraryTarget:'umd',
+        umdNamedDefine:false,
         path:path.resolve(__dirname,'./dist') //指定生成的文件目录
     },
     mode:'production',
-    // module:{
-    //     rules: [
-    //         {
-    //             test: /\.js$/,
-    //             loader: 'babel-loader',
-    //             exclude: /node_modules/
-    //         }
+    module:{
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }
 
-    //     ]
-    // }
+        ]
+    }
 }
