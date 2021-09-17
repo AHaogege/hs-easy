ac7b0d15de4399bcd4e2748ae77aa67d1561767e
AHaogege <1119515353@qq.com>
2021-09-17 21:48:56 +0800

    提交修改

--- src/common.js
+++ src/common.js
@@ -3,17 +3,17 @@ const deepCopy = (object)=>{
     return JSON.parse(JSON.stringify(object))
 }
 // localstorage 存取
-const getStorage = (name)=>{
-    return JSON.parse(window.localStorage.getItem(name))||''
-}
-// 设置
-const setStorage = (name,data)=>{
-    window.localStorage.setItem(name,JSON.stringify(data))
-}
-// 清除指定
-const removeStorage =(name)=>{
-    window.localStorage.removeItem(name)
-}
+// const getStorage = (name)=>{
+//     return JSON.parse(window.localStorage.getItem(name))||''
+// }
+// // 设置
+// const setStorage = (name,data)=>{
+//     window.localStorage.setItem(name,JSON.stringify(data))
+// }
+// // 清除指定
+// const removeStorage =(name)=>{
+//     window.localStorage.removeItem(name)
+// }
 module.exports = {
     deepCopy,
     getStorage,
